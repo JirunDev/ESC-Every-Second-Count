a46798dee3a4f27e9107877b06d8a4cc7853c22a
JirunDev <97425824+JirunDev@users.noreply.github.com>
2022-01-29 00:52:55 +0700

    [MAJOR] Add Primitive Gun System + Primitive Enemy System
    1. Added a Universal Gun Script for each bullet-type shooting (shoot, magazine, reload, spread, bullets per shot) (stats customizable)
    2. Added a low-poly Colt M1911 pistol model.
    3. Imported TextMesh Pro's Essentials assets + Unity Particle Pack asset
    4. Added primitive bullet holes system + particles
    5. Added static primitive dummy enemies with customizable health bar and damage system
    6. Added UI Interface for bullet counts + temporary Pointer
    
    *Known Bugs*
    1. When flying and shooting straight down, chances are you may hit yourself. I don't think it'd take much time but I'm just too lazy to fix it now.

---
+++ ESC Every Second Count/Assets/Scripts/CamShake.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamShake : MonoBehaviour
+{
+    public IEnumerator Shake (float duration, float magnitude)
+    {
+        Vector3 origPos = transform.localPosition;
+
+        float elapsed = 0.0f;
+        while(elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, origPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+
+}
