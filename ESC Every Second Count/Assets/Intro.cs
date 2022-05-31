812598642463911b739698249317837999f197a3
JirunDev <97425824+JirunDev@users.noreply.github.com>
2022-05-31 21:51:56 +0700

    31 #1

--- ESC Every Second Count/Assets/Intro.cs
+++ ESC Every Second Count/Assets/Intro.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Intro : MonoBehaviour
 {
     public GameObject intro;
     public GameObject mainMenu;
+    public VideoPlayer video;
 
     private int introCheck;
 
@@ -13,15 +15,21 @@ public class Intro : MonoBehaviour
     {
         //recover value
         introCheck = PlayerPrefs.GetInt("introcheck",0);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(introCheck != 0)
+        if (introCheck != 0)
         {
             intro.SetActive(false);
             mainMenu.SetActive(true);
         }
+        else
+        {
+            video.loopPointReached += EndReached;
+        }
+    }
+
+    public void EndReached(VideoPlayer vp)
+    {
+        intro.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
